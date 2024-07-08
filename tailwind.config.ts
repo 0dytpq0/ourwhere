bcbd551e2462fdd2db91820ed05990b292bbd87b
chasyuss <cotn963@gmail.com>
2024-07-08 20:55:11 +0900

    style: tailwind 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        //header-bg color
+        "header-color": "#53558F",
+        "button-color": "#D8BFF6",
+        //포스트페이지에 list color
+        "postpage-listcolor": "#9B9DCC",
+        //로그인페이지 bg color
+        "loginpage-color": "#F4F1FF",
+        //font
+        "font-color": "#333560",
+        //map에 숫자 bg color
+        "mapcircle-color": "#8085F2",
+      },
     },
   },
   plugins: [],
