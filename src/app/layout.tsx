7b46b36c3a83fdd3de760e0bad9ff7aaaf157ab0
chasyuss <cotn963@gmail.com>
2024-07-09 16:23:18 +0900

    feat: header 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navigation from '@/components/monocules/navigation';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "ourwhere",
-  description: "친구들과 모임 일정 등을 공유하는 웹 플렛폼입니다!!!!",
+  title: 'ourwhere',
+  description: '친구들과 모임 일정 등을 공유하는 웹 플렛폼입니다!!!!'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/monocules/navigation.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export default function Navigation() {
+  const buttons = [
+    { text: '로그인', href: '/login' },
+    { text: '회원가입', href: '/signup' }
+  ];
+  return (
+    <>
+      <nav className="sticky top-0 left-0 w-full z-10 h-16 bg-header-color">
+        <ul className="text-white h-16 flex items-center  m-auto px-4">
+          <Link href="/" className="font-lg font-bold">
+            {' '}
+            OURWHERE{' '}
+          </Link>
+          <div className="ml-auto flex space-x-4 mx-4">
+            {buttons.map((button, index) => (
+              <Link key={index} href={button.href}>
+                <button className="border-solid bg-loginpage-color text-font-color px-3.5 py-1 rounded-lg">
+                  {button.text}
+                </button>
+              </Link>
+            ))}
+          </div>
+        </ul>
+      </nav>
+    </>
+  );
+}
