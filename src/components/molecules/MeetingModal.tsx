9d21d59977dfe13bcffba47cc80af55299f00cff
Boyoung Lee <genuss040822@gamil.com>
2024-07-10 00:01:34 +0900

    fix : MeetingModal.tsx ui 변경 및 스타일 변경

--- src/components/molecules/MeetingModal.tsx
+++ src/components/molecules/MeetingModal.tsx
@@ -9,19 +9,16 @@ interface MeetingModalProps {
 const MeetingModal: React.FC<MeetingModalProps> = ({ handleClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-10 rounded-lg shadow-lg ">
+      <div className="bg-white p-24 rounded-[2.5rem] shadow-lg w-[28rem] ">
+        <h2 className="mx-auto w-fit text-2xl font-bold">새 모임 만들기</h2>
         <div className="flex flex-col space-y-4 ">
-          <PlaceSearch label="검색" type="text" placeholder="장소 검색" />
-          <InputField label="장소" type="text" placeholder="장소" />
-          <InputField label="주소" type="text" placeholder="주소" />
-          <InputField label="시간" type="time" />
-          <input
-            type="text"
-            placeholder="✍🏻 작성"
-            className=" p-3 h-[100px] bg-postpage-listcolor rounded-tr-lg rounded-bl-lg"
-          />
+          <InputField label="모임 이름" type="text" placeholder="모임 이름을 입력해주세요" />
+          <label>시작 날짜</label>
+          <input type="date" />~<label>종료 날짜</label>
+          <input type="date" />
+          <InputField label="비밀번호" type="password" placeholder="****" />
           <button className="bg-button-color text-loginpage-color p-1 rounded-xl" onClick={handleClose}>
-            추가하기
+            생성하기
           </button>
         </div>
       </div>
