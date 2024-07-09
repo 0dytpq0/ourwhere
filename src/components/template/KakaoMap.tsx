4d65becf474a2cd85de49ed634189ff1550c1223
slsl2 <kimminji7153@gmail.com>
2024-07-09 20:56:04 +0900

    meeting 페이지 카카오맵 & 내 위치 띄움

---
+++ src/components/template/KakaoMap.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import useGeoLocation from '@/lib/hooks/Geolocation';
+import Script from 'next/script';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services,clusterer&autoload=false`;
+
+const KakaoMap = () => {
+  const myLocation = useGeoLocation();
+  return (
+    <>
+      <section className="h-lvh mr-1">
+        <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+        <Map
+          center={
+            myLocation ? { lat: myLocation.latitude, lng: myLocation.longitude } : { lat: 33.450701, lng: 126.570667 }
+          }
+          style={{ width: '800px', height: '100%' }}
+        >
+          {myLocation && (
+            <MapMarker
+              position={{ lat: myLocation.latitude, lng: myLocation.longitude }}
+              image={{ src: '/my-location-icon.png', size: { width: 70, height: 70 } }}
+              title="현재 위치"
+            />
+          )}
+        </Map>
+      </section>
+    </>
+  );
+};
+
+export default KakaoMap;
