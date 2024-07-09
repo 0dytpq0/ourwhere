0f822f623090e11cb38b1203b06687af9d46a87b
chasyuss <cotn963@gmail.com>
2024-07-09 17:51:49 +0900

    feat: footer 추가

---
+++ src/components/molecules/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className=" bg-header-color text-white text-center py-4 fixed bottom-0 left-0 w-full z-10">
+      {' '}
+      &copy; 2024 독수리오남매. All rights reserved.
+    </footer>
+  );
+}
