43192f8f801219e4a39ee3783bfde5a4e2db528b
seongbin1013 <plok82890@gmail.com>
2024-07-10 09:47:18 +0900

    fix : 미팅 ui 수정

--- src/components/template/Meeting.tsx
+++ src/components/template/Meeting.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 import Modal from '../molecules/Modal';
+import Schedule from '../molecules/schedule';
 
 export default function Meeting() {
   const [modal, setModal] = useState(false);
@@ -8,10 +9,11 @@ export default function Meeting() {
   console.log(modal);
   return (
     <>
-      <section className="bg-loginpage-color h-lvh">
-        <div className="flex flex-col items-center">
+      <section className="bg-loginpage-color pt-16 h-dvh">
+        <div className="flex flex-col items-center pt-10">
           <h1 className="text-4xl mb-3 text-font-color">예은이 생파 홍대</h1>
           <input type="date" placeholder="날짜" className="p-1 w-64 rounded-xl" />
+          <Schedule />
           <button
             onClick={handleModal}
             className="w-16 h-16 rounded-full bg-header-color text-loginpage-color text-4xl mt-5 "
