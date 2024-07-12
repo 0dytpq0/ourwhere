99e0358a65353e4fcff5492febdb28e5992dc4e4
seongbin1013 <plok82890@gmail.com>
2024-07-12 21:26:24 +0900

    chore : 코드 저장

--- src/components/template/ScheduleModal.tsx
+++ src/components/template/ScheduleModal.tsx
@@ -8,7 +8,6 @@ const Modal = ({ schedule, onClose }) => {
     onClose;
     toggleScheduleModal();
   };
-
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10">
       <div className="bg-white p-10 rounded-lg shadow-lg ">
