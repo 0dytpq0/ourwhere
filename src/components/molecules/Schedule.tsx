f900c4de5d12290406cf9056bbbd2ac452154f24
seongbin1013 <plok82890@gmail.com>
2024-07-12 11:13:40 +0900

    feat : 스케줄 api 추가

--- src/components/molecules/Schedule.tsx
+++ src/components/molecules/Schedule.tsx
@@ -1,26 +1,18 @@
 'use client';
-import { createClient } from '@/supabase/client';
+import ScheduleAPI from '@/api/schedule.api';
+import { Tables } from '@/types/supabase';
 import { useEffect, useState } from 'react';
 
-interface ScheduleItem {
-  id: number;
-  time: string;
-  place: string;
-  address: string;
-  content: string;
-  uuid: number;
-  meetingId: number;
-}
-
-const supabase = createClient();
-
 function Schedule() {
-  const [scheduleData, setScheduleData] = useState<ScheduleItem[]>([]);
+  const [scheduleData, setScheduleData] = useState<Tables<'schedule'>[]>();
   const [error, setError] = useState<string | null>(null);
+
+  const scheduleAPI = new ScheduleAPI();
+  const user = localStorage.getItem('auth-storage');
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase.from('schedule').select();
+      const { data, error } = await scheduleAPI.selectUserSchedule(user?.state.user.id);
       if (error) {
         setError(error.message);
       } else {
@@ -37,6 +29,7 @@ function Schedule() {
           <div className="flex items-center justify-between space-y-8">
             <div className="flex items-center">
               <div className="w-8 h-8 rounded-full bg-purple-300 flex items-center justify-center text-white">
+                {/* 시간 순서대로 */}
                 {index + 1}
               </div>
               <div className="ml-2 text-purple-500">{items.time}</div>
