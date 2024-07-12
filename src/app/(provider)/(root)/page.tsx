34da0d4b9895ee83309fe80ffa7e44365335abfc
slsl2 <kimminji7153@gmail.com>
2024-07-12 20:21:44 +0900

    fix: 해결 못 함

--- src/app/(provider)/(root)/page.tsx
+++ src/app/(provider)/(root)/page.tsx
@@ -20,16 +20,17 @@ export default function Home() {
     }
     toggleMeetingModal();
 
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-center space-y-6">
-        <h2 className="text-2xl font-bold">OURWHERE</h2>
-        <p className="mt-4"> 소개 글</p>
-        <button className="mt-4 px-4 py-2 bg-button-color text-white rounded-lg" onClick={handleOpenModal}>
-          새 모임 생성하기!
-        </button>
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center space-y-6">
+          <h2 className="text-2xl font-bold">OURWHERE</h2>
+          <p className="mt-4"> 소개 글</p>
+          <button className="mt-4 px-4 py-2 bg-button-color text-white rounded-lg" onClick={handleOpenModal}>
+            새 모임 생성하기!
+          </button>
+        </div>
+        {isMeetingModalOpen && <MeetingModal />}
       </div>
-      {isMeetingModalOpen && <MeetingModal />}
-    </div>
-  );
+    );
+  };
 }
