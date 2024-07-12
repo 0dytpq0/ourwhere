3efe32a1d32af23e2770904801a234be65546d13
slsl2 <kimminji7153@gmail.com>
2024-07-12 20:25:05 +0900

    docs: 괄호 수정

--- src/app/(provider)/(root)/page.tsx
+++ src/app/(provider)/(root)/page.tsx
@@ -19,18 +19,17 @@ export default function Home() {
       return;
     }
     toggleMeetingModal();
-
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="text-center space-y-6">
-          <h2 className="text-2xl font-bold">OURWHERE</h2>
-          <p className="mt-4"> 소개 글</p>
-          <button className="mt-4 px-4 py-2 bg-button-color text-white rounded-lg" onClick={handleOpenModal}>
-            새 모임 생성하기!
-          </button>
-        </div>
-        {isMeetingModalOpen && <MeetingModal />}
-      </div>
-    );
   };
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center space-y-6">
+        <h2 className="text-2xl font-bold">OURWHERE</h2>
+        <p className="mt-4"> 소개 글</p>
+        <button className="mt-4 px-4 py-2 bg-button-color text-white rounded-lg" onClick={handleOpenModal}>
+          새 모임 생성하기!
+        </button>
+      </div>
+      {isMeetingModalOpen && <MeetingModal />}
+    </div>
+  );
 }
