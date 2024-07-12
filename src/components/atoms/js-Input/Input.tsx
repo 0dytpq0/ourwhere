b3703b532343897c00fa6d500bf479384378f803
Joseph Park <rkfnahs12e@gmail.com>
2024-07-12 11:52:11 +0900

    feat : input label 추가

--- src/components/atoms/js-Input/Input.tsx
+++ src/components/atoms/js-Input/Input.tsx
@@ -4,13 +4,14 @@ type InputProps = {
   label?: string;
   required?: boolean;
   identity?: string;
+  warnning?: string;
 } & ComponentProps<'input'>;
 
 const loginStyle = 'border border-gray-300 rounded-xl px-4 py-2.5 focus:outline-none focus:border-gray-950 transition';
 const modalStyle = '';
 const editStyke = '';
 
-function Input({ label, required, id, identity, ...props }: InputProps) {
+function Input({ label, required, id, identity, warnning, ...props }: InputProps) {
   const inputUid = useId();
   const inputId = id || inputUid;
 
@@ -23,15 +24,20 @@ function Input({ label, required, id, identity, ...props }: InputProps) {
 
   return (
     <div className="flex flex-col gap-y-1.5 [&+&]:mt-8 w-full">
+      {label && identity !== 'login' && (
+        <label htmlFor={inputId} className={`text-sm font-semibold ${label && 'h-4'}`}>
+          {label}
+        </label>
+      )}
+      <input id={inputId} {...props} className={inputStyle} />
       <label htmlFor={inputId} className={`text-sm font-semibold ${label && 'h-4'}`}>
-        {label && required && (
+        {warnning && required && (
           <span className="text-xs font-semibold text-gray-400 p-1">
-            {label}
+            {warnning}
             <span className="text-red-500">*</span>{' '}
           </span>
         )}
       </label>
-      <input id={inputId} {...props} className={inputStyle} />
     </div>
   );
 }
